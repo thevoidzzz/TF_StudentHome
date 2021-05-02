f3b31fb3abc93c3dbf446abf109fb15253e08cc0
davidRonald24 <david_165_19r@hotmail.com>
2021-05-02 11:37:28 -0500

    Region service

--- src/main/java/pe/edu/upc/StudentHome/service/IRegionService.java
+++ src/main/java/pe/edu/upc/StudentHome/service/IRegionService.java
@@ -1,5 +1,13 @@
 package pe.edu.upc.StudentHome.service;
 
+import java.util.List;
+
+import pe.edu.upc.StudentHome.models.entities.Region;
+
 public interface IRegionService {
 
-}
+    public void insert(Region reg);
+
+    List<Region> list();
+
+    public void delete(int idRegion);
