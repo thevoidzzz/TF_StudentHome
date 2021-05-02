bc7ccfadea326f44807f77537c55f0a84bdb164b
Anthony26974 <joel.2001.xd@gmail.com>
2021-05-02 11:20:35 -0500

    anthony commit

--- src/main/java/pe/edu/upc/StudentHome/service/ISubscriptionService.java
+++ src/main/java/pe/edu/upc/StudentHome/service/ISubscriptionService.java
@@ -1,5 +1,14 @@
 package pe.edu.upc.StudentHome.service;
 
+import java.util.List;
+
+import pe.edu.upc.StudentHome.models.entities.Subscription;
+
 public interface ISubscriptionService {
 
+    public void insert(Subscription sub);
+
+    List<Subscription> list();
+
+    public void delete(int idSubscription);
 }
